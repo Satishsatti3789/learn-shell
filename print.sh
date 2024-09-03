c096f48a59129c41bd765fbcf45d0a06f398b77a
Admin <satishdevops3789@gmail.com>
2024-09-03 06:43:06 +0530

    frontend script

---
+++ print.sh
@@ -0,0 +1,6 @@
+# To print a message there arecouple of commands, But echo is widely used one
+ echo Hello World
+
+ # While printing some times to grab the attention of user we might need to print in some color
+
+ # Syntax: echo -e "\e[COLmMESSAGE\e[0m"
